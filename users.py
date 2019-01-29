2b0feec593f29de3ff3220572f8e7ef5e49317e3
Mugo-Ndungu <twinnymugo@gmail.com>
2019-01-29 21:02:24 +0300

    Add users class and function save user

---
+++ users.py
@@ -0,0 +1,27 @@
+import pyperclip
+
+class User:
+    '''
+    User class for user input 
+    '''
+
+    user_list = [] # User Empty list
+
+    def __init__(self, first_name, last_name, phone_number, email):        
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+
+
+    def save_user(self):
+
+        '''
+        save_usermethod saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
+
+
+        
