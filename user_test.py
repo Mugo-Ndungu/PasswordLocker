75635632ec769d2632bfaa89ea0e4a140c4d4e7b
Mugo-Ndungu <twinnymugo@gmail.com>
2019-01-29 20:50:08 +0300

    Add test delete user to test if a user can delete a record

--- user_test.py
+++ user_test.py
@@ -66,4 +66,16 @@ class TestUser(unittest.TestCase):
 
     
     
+    def test_delete_user(self):
+            '''
+            test_delete_user to test if we can remove a user from our user list
+            '''
+            self.new_user.save_user()
+            test_user = User("Test","user","0712345678","test@user.com") # new user
+            test_user.save_user()
+
+            self.new_user.delete_user()# Deleting a user object
+            self.assertEqual(len(User.user_list),1)
+
+
 
