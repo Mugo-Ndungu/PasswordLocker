c7e52435091decc0ec60837aac58822c3ea382f2
Mugo-Ndungu <twinnymugo@gmail.com>
2019-01-29 20:46:06 +0300

    Add test to check whether inputs are initialized

--- user_test.py
+++ user_test.py
@@ -31,3 +31,15 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_user = User("James","Muriuki","0712345678","james@ms.com") # create user object
+
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"James")
+        self.assertEqual(self.new_user.phone_number,"0712345678")
+        self.assertEqual(self.new_user.email,"james@ms.com")
+        self.assertEqual(self.new_user.last_name,"Muriuki")
