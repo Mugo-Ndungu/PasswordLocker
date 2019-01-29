21e98812c4f8ddad6347c691e92d2fd9aa0f0cf2
Mugo-Ndungu <twinnymugo@gmail.com>
2019-01-29 20:56:19 +0300

    Add test copy email to copy new users email

--- user_test.py
+++ user_test.py
@@ -115,3 +115,17 @@ class TestUser(unittest.TestCase):
 
         self.assertEqual(User.display_user(),User.user_list)
 
+
+    def test_copy_email(self):
+        '''
+        Test to confirm that we are copying the email address from a found user
+        '''
+
+        self.new_user.save_user()
+        User.copy_email("0712345678")
+
+        self.assertEqual(self.new_user.email,pyperclip.paste())
+
+
+if __name__ == '__main__':
+    unittest.main()
